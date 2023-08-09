9f044e260805136fbccd4f83483684dbdebdc522
aftermath0703 <3368760963@qq.com>
2023-08-09 16:18:16 +0800

    init

---
+++ models/resnet101.py
@@ -0,0 +1,97 @@
+import torch
+import torch.nn as nn
+
+
+# Define the BasicBlock for ResNet
+class BasicBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(out_channels)
+            )
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        out += self.shortcut(identity)
+        out = self.relu(out)
+
+        return out
+
+
+# Define the ResNet101 model
+class ResNet101(nn.Module):
+    def __init__(self, num_classes=7):
+        super(ResNet101, self).__init__()
+        self.in_channels = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self._make_layer(64, 3)
+        self.layer2 = self._make_layer(128, 4, stride=2)
+        self.layer3 = self._make_layer(256, 23, stride=2)
+        self.layer4 = self._make_layer(512, 3, stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512, num_classes)
+
+    def _make_layer(self, out_channels, num_blocks, stride=1):
+        layers = []
+        layers.append(BasicBlock(self.in_channels, out_channels, stride))
+        self.in_channels = out_channels
+
+        for _ in range(1, num_blocks):
+            layers.append(BasicBlock(out_channels, out_channels))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.maxpool(out)
+
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+
+        out = self.avgpool(out)
+        out = torch.flatten(out, 1)
+        out = self.fc(out)
+
+        return out
+
+
+# # Create the model
+# model = ResNet101(num_classes=7)
+# # Set the input shape
+# input_shape = (128, 3, 44, 44)
+# # Set the target shape
+# target_shape = (128,)
+# # Create random input and target tensors
+# inputs = torch.randn(input_shape)
+# targets = torch.randint(0, 1000, target_shape)
+# # Forward pass
+# outputs = model(inputs)
+# # Print the output shape
+# print(outputs.shape)
